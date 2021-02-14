fc6825e5ea48b39a6cf6343cd390b5dc0631097d
baptekar <79029587+baptekar@users.noreply.github.com>
2021-02-14 06:35:41 +0530

    Add files via upload

---
+++ m1i_lonovo_lib.js
@@ -0,0 +1,12 @@
+let m1i = {};
+
+m1i.enterprise ={
+	init: () =>{
+		
+	},
+	solution: () =>{
+		gb_page_btn_CTA_url: () =>{
+			jQuery('.changeURLOnload').attr('href','https://techtoday.lenovo.com/gb/en/workstations/m&e');
+		}
+	}
+}
